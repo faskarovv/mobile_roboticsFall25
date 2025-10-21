f3bfb58f886e3acd472804cfa5c4e11de573ab6d
faskarovv <fasgerov06@gmail.com>
2025-10-21 15:59:01 +0400

    added Lab1.py

--- Lab1.py
+++ Lab1.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env  pybricks-micropython
+#!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor, ColorSensor
 from pybricks.parameters import Port, Color
@@ -17,48 +17,53 @@ right_sensor = ColorSensor(Port.S3)
 # Configure the robot base
 robot = DriveBase(left_motor, right_motor, wheel_diameter=56, axle_track=114)
 
-# Movement constants
-drive_speed = 80          # forward speed
-hard_turn_speed = 90      # sharp turn rate
-slight_turn_speed = 30    # gentle bias when both sensors see green
+# ======= Tunable constants =======
+drive_speed = 80          # normal forward speed
+turn_speed = 90           # base turning rate
+slight_turn_speed = 30    # when both green (slight bias)
+recovery_turn_gain = 1.5  # how much stronger to turn when line is lost
+recovery_speed_gain = 0.5 # how much slower to go when line is lost
 
-last_turn = 0             # remembers last direction (-1 = left, 1 = right, 0 = straight)
-no_green_count = 0        # counts cycles where no green is seen
+last_turn = 0             # remembers last turn (-1 = left, 1 = right, 0 = straight)
+no_green_count = 0        # how long line has been lost
 
+# ======= Main loop =======
 while True:
-    # Read colors
     left_detected = left_sensor.color()
     right_detected = right_sensor.color()
 
     if left_detected == Color.GREEN and right_detected != Color.GREEN:
-        robot.drive(drive_speed, -hard_turn_speed)
+        # Turn left
+        robot.drive(drive_speed * 0.6, -turn_speed)
         last_turn = -1
         no_green_count = 0
 
     elif right_detected == Color.GREEN and left_detected != Color.GREEN:
-        robot.drive(drive_speed, hard_turn_speed)
+        # Turn right
+        robot.drive(drive_speed * 0.6, turn_speed)
         last_turn = 1
         no_green_count = 0
 
     elif left_detected == Color.GREEN and right_detected == Color.GREEN:
-        # go forward but with slight bias in the last direction
+        # Both sensors on tape → go forward but with slight bias
         robot.drive(drive_speed, last_turn * slight_turn_speed)
         no_green_count = 0
 
     else:
+        # Both sensors off → likely lost the line, recover
         no_green_count += 1
 
         if last_turn == -1:
-            robot.drive(drive_speed, -hard_turn_speed)
+            robot.drive(drive_speed * recovery_speed_gain, -turn_speed * recovery_turn_gain)
         elif last_turn == 1:
-            robot.drive(drive_speed, hard_turn_speed)
+            robot.drive(drive_speed * recovery_speed_gain, turn_speed * recovery_turn_gain)
         else:
-            robot.drive(drive_speed, 0)
+            robot.drive(drive_speed * 0.5, 0)
 
-        # optional: stop after being off tape for too long
-        if no_green_count > 100:  # roughly 1 second if wait(10)
+        # Optional: stop completely if lost too long
+        if no_green_count > 120:  # about 1.2 seconds
             robot.stop()
-            ev3.speaker.beep()  # signal that it lost the line
+            ev3.speaker.beep()
             break
 
     wait(10)
